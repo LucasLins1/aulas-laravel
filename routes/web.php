7f89eb00f36c0bfff7f49758e99311e18710f5a6
LUCAS FIGUEIREDO LINS <lucas.flins@senacsp.edu.br>
2025-06-09 11:42:35 -0300

    aula 01

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/sobrenos', function(){
-    return 'Essa é a pagina sobre nós';
-});
-
-Route::get('/contato', function(){
-    return 'Essa é a de contato';
-});
-
-Route::get('/apoie', function(){
-    return 'Essa é a de apoio ao projeto';
-});
-
-Route::get('/faq', function(){
-    return 'Essa é a FAQ';
-});
-
-Route::get('/redes', function(){
-    return 'Essa é a sobre nossas redes sociais';
+    return 'Essa é a pagina sobre nós'
 });
